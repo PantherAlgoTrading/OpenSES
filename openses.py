5e7cc24b0192bb162ee51c39a78dec78e1963e92
Rohit <rohit.ganguly@pitt.edu>
2021-01-07 18:45:17 -0800

    Added black code formatter to pipenv and formatted code

--- openses.py
+++ openses.py
@@ -4,39 +4,35 @@ import sys
 from configparser import ConfigParser
 from pathlib import Path
 
+
 def initialize_openses():
     cwd = Path()
-    openses_folder = cwd / '.openses'
-    strategies_folder = cwd / 'strategies'
-    pipelines_folder = cwd / 'pipelines'
+    openses_folder = cwd / ".openses"
+    strategies_folder = cwd / "strategies"
+    pipelines_folder = cwd / "pipelines"
 
     openses_folder.mkdir(exist_ok=True)
     strategies_folder.mkdir(exist_ok=True)
     pipelines_folder.mkdir(exist_ok=True)
 
-    config_file = cwd / 'config.ini'
+    config_file = cwd / "config.ini"
 
-    with config_file.open(mode='w') as f:
+    with config_file.open(mode="w") as f:
         config = ConfigParser()
-        config['OpenSES'] = {
-            'NumWorkers': '1',
-            'RenewalTime': '06:00 EST',
-            'OpenTime': '06:05 EST',
-            'CompletionTime': '06:10 EST',
-            'InstanceName': 'OpenSES Instance',
-            'DefaultFundAmount': '10000',
-            'StrategiesDirectory': 'strategies',
-            'PipelinesDirectory': 'pipelines'
+        config["OpenSES"] = {
+            "NumWorkers": "1",
+            "RenewalTime": "06:00 EST",
+            "OpenTime": "06:05 EST",
+            "CompletionTime": "06:10 EST",
+            "InstanceName": "OpenSES Instance",
+            "DefaultFundAmount": "10000",
+            "StrategiesDirectory": "strategies",
+            "PipelinesDirectory": "pipelines",
         }
         config.write(f)
 
 
-
-
-
-commands = {
-    'init': initialize_openses
-}
+commands = {"init": initialize_openses}
 
 if __name__ == "__main__":
     command = sys.argv[1]
